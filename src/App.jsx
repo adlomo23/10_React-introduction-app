0d0a4960680cc7f9d2996c4d9619c9c9a1dc6e12
adlomo23 <adlomo23@gmail.com>
2024-01-30 18:40:42 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+  const name = "Adrià Costa Parra";
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <h2>{`Aquest xaval és burro -> ${name}`}</h2>
+    </>
+  );
+}
+
+export default App;
