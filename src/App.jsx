bd43bad9a33cb2db078fab454525ee1c4b24d592
adlomo23 <adlomo23@gmail.com>
2024-02-06 20:11:20 +0100

    Finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Header } from "./Header";
+import { Total } from "./Total";
+import { Content } from "./Content";
 
-function App() {
-  const name = "Adrià Costa Parra";
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+
   return (
-    <>
-      <h1>Hello World!</h1>
-      <h2>{`Aquest xaval és burro -> ${name}`}</h2>
-    </>
+    <div>
+      <Header course={course} />
+
+      <Content
+        part1={part1}
+        part2={part2}
+        part3={part3}
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
+
+      <Total
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
+    </div>
   );
-}
+};
 
 export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,5 @@
+export const Header = (props) => {
+  const course = props.course;
+
+  return <h1>{course}</h1>;
+};
